9db1fa44e5065501d91faddd8e74c369274839d7
Anton Novojilov <andy@essentialkaos.com>
2024-11-03 07:44:32 +0300

    Add StackBlur

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package imaging
 
 import (
 	"image"
+	"image/color"
 	"math"
 	"runtime"
 	"sync"
@@ -87,15 +88,77 @@ func reverse(pix []uint8) {
 	}
 }
 
+// toNRGBA converts an image type to *image.NRGBA with min-point at (0, 0).
 func toNRGBA(img image.Image) *image.NRGBA {
-	if img, ok := img.(*image.NRGBA); ok {
-		return &image.NRGBA{
-			Pix:    img.Pix,
-			Stride: img.Stride,
-			Rect:   img.Rect.Sub(img.Rect.Min),
+	srcBounds := img.Bounds()
+	if srcBounds.Min.X == 0 && srcBounds.Min.Y == 0 {
+		if src0, ok := img.(*image.NRGBA); ok {
+			return src0
 		}
 	}
-	return Clone(img)
+	srcMinX := srcBounds.Min.X
+	srcMinY := srcBounds.Min.Y
+
+	dstBounds := srcBounds.Sub(srcBounds.Min)
+	dstW := dstBounds.Dx()
+	dstH := dstBounds.Dy()
+	dst := image.NewNRGBA(dstBounds)
+
+	switch src := img.(type) {
+	case *image.NRGBA:
+		rowSize := srcBounds.Dx() * 4
+		for dstY := 0; dstY < dstH; dstY++ {
+			di := dst.PixOffset(0, dstY)
+			si := src.PixOffset(srcMinX, srcMinY+dstY)
+			for dstX := 0; dstX < dstW; dstX++ {
+				copy(dst.Pix[di:di+rowSize], src.Pix[si:si+rowSize])
+			}
+		}
+	case *image.YCbCr:
+		for dstY := 0; dstY < dstH; dstY++ {
+			di := dst.PixOffset(0, dstY)
+			for dstX := 0; dstX < dstW; dstX++ {
+				srcX := srcMinX + dstX
+				srcY := srcMinY + dstY
+				siy := src.YOffset(srcX, srcY)
+				sic := src.COffset(srcX, srcY)
+				r, g, b := color.YCbCrToRGB(src.Y[siy], src.Cb[sic], src.Cr[sic])
+				dst.Pix[di+0] = r
+				dst.Pix[di+1] = g
+				dst.Pix[di+2] = b
+				dst.Pix[di+3] = 0xff
+				di += 4
+			}
+		}
+	case *image.Gray:
+		for dstY := 0; dstY < dstH; dstY++ {
+			di := dst.PixOffset(0, dstY)
+			si := src.PixOffset(srcMinX, srcMinY+dstY)
+			for dstX := 0; dstX < dstW; dstX++ {
+				c := src.Pix[si]
+				dst.Pix[di+0] = c
+				dst.Pix[di+1] = c
+				dst.Pix[di+2] = c
+				dst.Pix[di+3] = 0xff
+				di += 4
+				si++
+			}
+		}
+	default:
+		for dstY := 0; dstY < dstH; dstY++ {
+			di := dst.PixOffset(0, dstY)
+			for dstX := 0; dstX < dstW; dstX++ {
+				c := color.NRGBAModel.Convert(img.At(srcMinX+dstX, srcMinY+dstY)).(color.NRGBA)
+				dst.Pix[di+0] = c.R
+				dst.Pix[di+1] = c.G
+				dst.Pix[di+2] = c.B
+				dst.Pix[di+3] = c.A
+				di += 4
+			}
+		}
+	}
+
+	return dst
 }
 
 // rgbToHSL converts a color from RGB to HSL.
